870f5f4db5e2552c9e150ae52b4c02c296a18b57
AmanSingh <amansinghr884@gmail.com>
2022-12-20 22:50:34 +0530

    Enable Dark Theme

--- ComposeTutorial/app/src/main/java/com/gmail/amansighr884/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/gmail/amansighr884/composetutorial/MainActivity.kt
@@ -1,24 +1,34 @@
 package com.gmail.amansighr884.composetutorial
 
 import androidx.compose.foundation.Image
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.gmail.amansighr884.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            ComposeTutorialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
+            }
+
         }
     }
 }
@@ -30,23 +40,37 @@ fun MessageCard(msg: com.gmail.amansighr884.composetutorial.Message) {
     Row(modifier = Modifier.padding(all=8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
+            contentDescription = null,
             modifier = Modifier
-                // Set image size to 40 dp
                 .size(40.dp)
-                // Clip image to be shaped as a circle
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            androidx.compose.material.Surface(shape = MaterialTheme.shapes.medium,elevation=1.dp) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.body2)
+            }
         }
     }
 }
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 
 @Preview
 @Composable
